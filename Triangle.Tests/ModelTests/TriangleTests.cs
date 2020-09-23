8ca6790fd028cd3ba1c3f8d948c25c79a9a28d70
jd <duverseaujohnny21@gmail.com>
2020-09-22 23:13:16 -0400

    modified tringle.cs
    Co-Authored-By Michael Watts  <wattsjmichael@gmail.com>

--- Triangle.Tests/ModelTests/TriangleTests.cs
+++ Triangle.Tests/ModelTests/TriangleTests.cs
@@ -26,7 +26,7 @@ namespace Triangle.Tests
       Assert.AreEqual(true, testTriangle.IsTriangleTracker(100, 123, 232));
     }
     [TestMethod]
-    public void isTriangle_NotTriangle_true() 
+      public void isTriangle_NotTriangle_true() 
     {
       TriangleTracker testTriangle = new TriangleTracker();
       Assert.AreEqual(true, testTriangle.IsTriangleTracker(7, 6, 15));
